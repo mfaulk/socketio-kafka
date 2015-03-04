2da2ba731c0bc9e78875c678b065d1481960e021
Matt Faulkner <mnfaulk@gmail.com>
2015-03-04 11:34:27 -0800

    Uses socket.io to push Kafka messages to web clients

---
+++ README.md
@@ -0,0 +1,23 @@
+# socketio-kafka
+Uses Socket.io to push Kafka messages to clients. Based on the [socket.io chat demo](http://socket.io/get-started/chat/).
+
+## Requirements
+- Kafka. The [Apache Kafka Quick Start](http://socket.io/get-started/chat/) provides instructions on setting up a simple ZooKeeper server and Kafka broker.
+- [Node](http://nodejs.org) and npm
+
+## Running
+Start Kafka, e.g. open two shells and start zookeeper, then kafka:
+```
+kafka_2.10-0.8.2.0$ bin/zookeeper-server-start.sh config/zookeeper.properties
+```
+
+```
+kafka_2.10-0.8.2.0$ bin/kafka-server-start.sh config/server.properties
+```
+
+Within this project's directory, install the Node dependencies and run the app:
+```
+npm install
+node index.js
+```
+Then take a look at `localhost:3000`.
---
+++ index.js
@@ -0,0 +1,75 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+var kafka = require('kafka-node');
+
+var KAFKA_BROKER = 'localhost:2181';
+var KAFKA_CLIENT_ID = "socketio-kafka";
+var kafkaClient = new kafka.Client(KAFKA_BROKER, KAFKA_CLIENT_ID);
+var producer = new kafka.Producer(kafkaClient);
+var consumer = new kafka.Consumer(kafkaClient, [], {autoCommit: true});
+var topics = ['test-topic'];
+
+/* 
+	Uses a producer to create the topics that the consumer expects. 
+	This is a hack to avoid errors when a consumer adds a non-existant topic.
+*/
+function initializeTopics(topics) {
+  producer.createTopics(topics, false, function (err, data) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log('Created topics.');
+      console.log(data);
+    }
+
+    consumer.addTopics(topics, function (err, added) {
+      if (err) {
+        // If topics don't exist yet, an error is expected.
+        console.log(err);
+      } else {
+        console.log(added);
+      }
+    });
+
+  });
+}
+
+function sendMessage() {
+  var payloads = [
+    { topic: 'test-topic', messages: 'hi', partition: 0 }
+    ];
+  console.log('Sending...');
+  producer.send(payloads, function(err, data){
+    console.log('.');
+  });
+}
+
+producer.on('error', function (err) {
+  console.log("Producer", err);
+});
+
+producer.on('ready', function (message) {
+  initializeTopics(topics);  
+});
+
+producer.on('ready', function (message) {setInterval(sendMessage, 1000)});
+
+consumer.on('message', function (message) {
+  console.log('emitting message...');
+  io.emit('test-topic', 'message');
+});
+
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
